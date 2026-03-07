3d733f68cb107536cffd14fc963991980266502e
Matheus Marins <matheus.bernardelo@live.com>
2026-03-06 21:09:30 -0300

    fix(locadora): ajustes no workflow e implemento do teste

---
+++ locadora/tests/conftest.py
@@ -0,0 +1,64 @@
+import psycopg2
+import pytest
+import os
+
+@pytest.fixture(autouse=True)
+def clean_table():
+    conn = psycopg2.connect(
+        host="localhost",
+        database="locadora",
+        user="postgres",
+        password="postgres"
+    )
+
+    cur = conn.cursor()
+    cur.execute("TRUNCATE TABLE filmes RESTART IDENTITY;")
+    conn.commit()
+
+    cur.close()
+    conn.close()
+
+@pytest.fixture(scope="session", autouse=True)
+def setup_database():
+    conn = psycopg2.connect(
+        host=os.getenv("POSTGRES_HOST", "localhost"),
+        port=os.getenv("POSTGRES_PORT", 5432),
+        database=os.getenv("POSTGRES_DB", "locadora"),
+        user=os.getenv("POSTGRES_USER", "postgres"),
+        password=os.getenv("POSTGRES_PASSWORD", "postgres")
+    )
+
+    cur = conn.cursor()
+
+    cur.execute("""
+    CREATE EXTENSION IF NOT EXISTS unaccent;
+
+    CREATE TABLE IF NOT EXISTS filmes (
+        id SERIAL PRIMARY KEY,
+        titulo TEXT NOT NULL,
+        quantidade INTEGER NOT NULL
+    );
+    """)
+
+    conn.commit()
+    cur.close()
+    conn.close()
+
+@pytest.fixture(scope="session", autouse=True)
+def insert_data():
+    conn = psycopg2.connect(
+        host=os.getenv("POSTGRES_HOST", "localhost"),
+        port=os.getenv("POSTGRES_PORT", 5432),
+        database=os.getenv("POSTGRES_DB", "locadora"),
+        user=os.getenv("POSTGRES_USER", "postgres"),
+        password=os.getenv("POSTGRES_PASSWORD", "postgres")
+    )
+
+    cur = conn.cursor()
+
+    cur.execute("INSERT INTO filmes(titulo,quantidade) VALUES(TESTE,1);")
+
+    conn.commit()
+    cur.close()
+    conn.close()
+
